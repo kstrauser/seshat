e7d3400d73ce5ad7e228f77a2956f19efdb53c47
Kirk Strauser <kirk@strauser.com>
2011-05-31 15:15:30 -0500

    Updated to version 0.3.0
    Also updated the url, and shows support for Python 2.4.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name = 'Seshat',
-    version = '0.2.1',
+    version = '0.3.0',
     packages=['seshat'],
     description = 'Sessioned chat system linking Ajax web interfaces with internal Jabber services',
     author='Kirk Strauser',
     author_email='kirk@strauser.com',
-    url='http://kstrauser.github.com/seshat/',
+    url='https://github.com/kstrauser/seshat',
     long_description='Seshat provides a simple method for adding real-time chat windows to your (Pyramid) web applications so that visitors can easily talk to any of a configured set of Jabber users. An example use would be allowing customers to chat with internal customer service representatives. The name is short for "sessioned chat", and resemblence to the Egyptian goddess of wisdom, knowledge, and writing is coincidental.',
     keywords=['jabber', 'xmpp', 'chat', 'pyramid', 'ajax'],
     classifiers=[
@@ -20,6 +20,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.4',
         'Programming Language :: Python :: 2.5',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
